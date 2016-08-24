fecae30bf64c4c919d32ece1297d401cf83e6558
ToTranMinhNhut <ttmnhut93@gmail.com>
2016-08-24 11:05:15 +0700

    update

---
+++ Chapter1AbtractInterface/src/Test/ShapTestRuner.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package Test;
+
+/**
+ * @author hv
+ *
+ */
+public class ShapTestRuner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if (true) {
+			System.out.print("sdfsf");
+		} else {
+			//sdflkjsjlkd
+		}
+		
+	}
+
+}
---
+++ Chapter1AbtractInterface/src/Test/ShapeTestsuite.java
@@ -0,0 +1,12 @@
+package Test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CircleJUnitTest.class, RectangleJUnitTest.class,
+		SquareJUnitTest.class })
+public class ShapeTestsuite {
+
+}
