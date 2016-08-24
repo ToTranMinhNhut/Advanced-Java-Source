1f82d1b767b45e689db40e58a651c72e6ba57f0b
ToTranMinhNhut <ttmnhut93@gmail.com>
2016-08-24 11:20:09 +0700

    update test

--- Chapter1AbtractInterface/src/Test/ShapeTestRuner.java
+++ Chapter1AbtractInterface/src/Test/ShapeTestRuner.java
@@ -1,31 +1,27 @@
-/**
- * 
- */
 package Test;
 
-/**
- * @author hv
- *
- */
 public class ShapeTestRuner {
 
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        if (true) {
-            //sdflkjkl
+        
+        if () {
+            fsdjlkfj
         } else {
-            System.out.print("");
+            int fjlksjf
+            
+            if () {
+                sdflkj
+                
+                if () {
+                    sadlfkj
+                } else {
+                    fsdfl;k
+                }
+            }
         }
         
-        while() {
-            System;
-        }
-            sdlkjflk
         
-       
     }
 
 }
