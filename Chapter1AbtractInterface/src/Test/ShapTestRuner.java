43fcb00c4fed03420fda98d7895c8999d0af551c
ToTranMinhNhut <ttmnhut93@gmail.com>
2016-08-24 11:33:37 +0700

    update testsuite

---
+++ Chapter1AbtractInterface/src/Test/ShapTestRuner.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package Test;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+
+/**
+ * @author hv
+ *
+ */
+public class ShapTestRuner {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Result result = JUnitCore.runClasses(ShapeTestsuite.class);
+
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        
+        System.out.println(result.wasSuccessful());    
+    }
+
+}
